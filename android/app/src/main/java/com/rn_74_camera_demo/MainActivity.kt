a90e19602b3061930c47568be624093fd580b3d3
ZhenQian <zhenqian@keyst.one>
2024-08-14 20:28:38 +0800

    update: integrage expo with react native test pass

--- android/app/src/main/java/com/rn_74_camera_demo/MainActivity.kt
+++ android/app/src/main/java/com/rn_74_camera_demo/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.rn_74_camera_demo
+import expo.modules.ReactActivityDelegateWrapper
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -23,5 +24,5 @@ class MainActivity : ReactActivity() {
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+      ReactActivityDelegateWrapper(this, BuildConfig.IS_NEW_ARCHITECTURE_ENABLED, DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled))
 }
