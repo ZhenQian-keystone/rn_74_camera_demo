44d20b012c966125dbf7146abb2b0ea068861e3a
ZhenQian <zhenqian@keyst.one>
2024-08-15 10:34:00 +0800

    update: add config commnent for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  // must config react hooks use rule
   rules: {
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': [
